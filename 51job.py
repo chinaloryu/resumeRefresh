0c1873e01dedb3276ce84f89bb0c482576b66f17
loryu <chinaloryu@gmail.com>
2020-03-19 23:42:24 +0800

    #@20200319 send warning to dingtalk after failed

--- 51job.py
+++ 51job.py
@@ -67,5 +67,3 @@ if __name__ == '__main__':
 		api_url = "https://oapi.dingtalk.com/robot/send?access_token=%s" % sys.argv[4]
 		if 0 < rtn:
 			sendMsg('[' + time.ctime() + ']' + 'resume refresh failed.', sys.argv[3])
-		else:
-			sendMsg('[' + time.ctime() + ']' + 'resume refresh success.', sys.argv[3])
