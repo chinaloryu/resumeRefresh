dddc71bd0e2a785d44ddc8879e4cbd3bd6fa77af
loryu <chinaloryu@gmail.com>
2020-03-19 23:35:38 +0800

    #@20200319 send result to dingtalk

--- 51job.py
+++ 51job.py
@@ -1,45 +1,71 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-#Author loryu
+# Author loryu
 from selenium import webdriver
 from time import sleep
-import os,sys
-import time,platform
+import os, sys
+import time, platform
+import requests, json
 
-def refresh(loginname,password,system_os):
-	#executable binary of chromedriver
+
+def sendMsg(msg, phone_num):
+	json_text = {
+		"msgtype": "text",
+		"at": {
+			"atMobiles": [
+			phone_num
+			],
+			"isAtAll": False
+		},
+		"text": {
+			"content": msg
+		}
+	}
+	print(requests.post(api_url, json.dumps(json_text), headers=headers).content)
+
+
+def refresh(loginname, password, system_os):
+	# executable binary of chromedriver
 	if system_os == 'Windows':
 		chrome_driver = './chromedriver.exe'
 	elif system_os == 'Linux':
 		chrome_driver = './chromedriver'
 	elif system_os == 'Darwin':
 		chrome_driver = './chromedriver_darwin'
-	else :
+	else:
 		print('Not surpported OS!')
 	options = webdriver.ChromeOptions()
 	options.add_argument('headless')
 	options.add_argument('no-sandbox')
 	options.add_argument('disable-dev-shm-usage')
 	# chrome_driver = './chromedriver'
-	driver = webdriver.Chrome(executable_path = chrome_driver,options = options)
+	driver = webdriver.Chrome(executable_path=chrome_driver, options=options)
 	# driver = webdriver.Chrome()
-	print('[',time.ctime(),']','start refresh resume......')
+	print('[', time.ctime(), ']', 'start refresh resume......')
 	try:
-	    driver.get('http://login.51job.com')
-	    driver.find_element_by_id('loginname').send_keys(loginname)
-	    driver.find_element_by_id('password').send_keys(password)
-	    test=driver.find_element_by_id('login_btn').click()
-	    driver.get('http://i.51job.com/userset/my_51job.php')
-	    driver.find_element_by_xpath("//span[text()=\"刷新\"]").click()
-	    print('[',time.ctime(),']','refresh success.')
+		driver.get('http://login.51job.com')
+		driver.find_element_by_id('loginname').send_keys(loginname)
+		driver.find_element_by_id('password').send_keys(password)
+		driver.find_element_by_id('login_btn').click()
+		driver.get('http://i.51job.com/userset/my_51job.php')
+		driver.find_element_by_xpath("//span[text()=\"刷新\"]").click()
+		print('[', time.ctime(), ']', 'refresh success.')
+		return 0
 	except:
-	    print('[',time.ctime(),']','refresh failed.')
+		print('[', time.ctime(), ']', 'refresh failed.')
+		return 1
 	driver.close()
 
 if __name__ == '__main__':
-	if len(sys.argv) < 3:
-		print('insufficient arguments, need 2, actually {}'.format(len(sys.argv) - 1))
-		print('Usage:python ' + sys.argv[0] + ' <loginname> <password>')
+	if len(sys.argv) < 5:
+		print('insufficient arguments, need 4, actually {}'.format(len(sys.argv) - 1))
+		print('Usage:python ' + sys.argv[0] + ' <login name> <password> <phone number> <access token>')
 	else:
 		sys_os = platform.system()
-		refresh(sys.argv[1],sys.argv[2],sys_os)
+		rtn = refresh(sys.argv[1], sys.argv[2], sys_os)
+		headers = {'Content-Type': 'application/json;charset=utf-8'}
+		api_url = "https://oapi.dingtalk.com/robot/send?access_token=%s" % sys.argv[4]
+		if 0 < rtn:
+			sendMsg('[' + time.ctime() + ']' + 'resume refresh failed.', sys.argv[3])
+		else:
+			sendMsg('[' + time.ctime() + ']' + 'resume refresh success.', sys.argv[3])
