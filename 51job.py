66cdbbe87c67810c9a6620e722612ce000742d68
mac <chinaloryu@gmail.com>
2021-07-29 11:05:44 +0800

    #@20210729 add sleep time for login

--- 51job.py
+++ 51job.py
@@ -38,7 +38,7 @@ def refresh(loginname, password, system_os):
 	options = webdriver.ChromeOptions()
 	options.add_argument('headless')
 	options.add_argument('window-size=1920x1080')
-	# options.add_argument('start-maximized')
+	options.add_argument('start-maximized')
 	options.add_argument('no-sandbox')
 	options.add_argument('disable-dev-shm-usage')
 	# chrome_driver = './chromedriver'
@@ -59,6 +59,7 @@ def refresh(loginname, password, system_os):
 		print('>>>>>>>>>>>>>\nfind password text frame failed!')
 	try:
 		driver.find_element_by_id('login_btn_withPwd').click()
+		sleep(2)
 	except:
 		print('>>>>>>>>>>>>>\nfind login button failed!')
 	try:
